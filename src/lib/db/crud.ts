dd181a39458a3243620d86d637f710f1803ddaf4
MeowcaTheoRange <tabletmeowcat@gmail.com>
2023-09-15 00:59:12 -0500

    sigh, what the fuck webpack

--- src/lib/db/crud.ts
+++ src/lib/db/crud.ts
@@ -1,5 +1,5 @@
-import { mainDB } from "@/lib/db/mongodb";
 import { FindCursor, Filter as MDBFilter, ObjectId, Sort } from "mongodb";
+import { mainDB } from "./mdb";
 
 export type WithId<TSchema> = Omit<TSchema, "_id"> & {
   _id: ObjectId;
